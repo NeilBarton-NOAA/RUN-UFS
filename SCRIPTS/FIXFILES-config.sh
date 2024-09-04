b7995105e66921874cffad68fcec5cce095d2c83
NeilBarton-NOAA <neil.barton@noaa.gov>
2024-09-04 16:14:41 -0500

    add old co2 data

--- SCRIPTS/FIXFILES-config.sh
+++ SCRIPTS/FIXFILES-config.sh
@@ -29,5 +29,10 @@ if [[ ${POSTXCONFIG:-F} != F ]]; then
     ln -sf ${POSTXCONFIG} postxconfig-NT.txt
     ln -sf ${POSTXCONFIG} postxconfig-NT_FH00.txt
 fi
+if (( ${DTG:0:4} < 2009 )); then
+    for Y in $(seq ${DTG:0:4} 2008); do
+        ln -sf /work/noaa/global/glopara/fix/am/20220805/co2dat_4a/global_co2historicaldata_${DTG:0:4}.txt co2historicaldata_${DTG:0:4}.txt
+    done
+fi
 # remove any broken links
 find . -xtype l -exec rm {} \;
